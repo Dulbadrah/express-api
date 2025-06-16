7c1f895efab59eb5d15bbd05e4089ed3abafed81
25LP5306 25LP5306 <25LP5306@pinecones-iMac.local>
2025-06-16 12:32:46 +0800

    mmvdso

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,58 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import fs from "fs-extra";
+import { User } from "./type/type";
 
 const app = express();
-const port = 413;
-
+const port = 1108;
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send({
-    name: "movie-app",
-    id: "578438390",
-    age: "21",
+    name: "testName",
+    age: "32",
+    id: 13,
   });
 });
 
-app.post("/user", (req, res) => {
-  const { name, age } = req.body;
+app.post("/user", (req: Request, res: Response) => {
+  const { name, age }: { name: string; age: number } = req.body;
+  res.json({ message: `User  ${name} is ${age} years old.` });
+});
 
-  res.json({ messege: `user ${name} is ${age} years old.` });
+app.put("/updateUser", (req: Request, res: Response) => {
+  const { name, age }: { name: string; age: number } = req.body;
+  res.send(`updated user ${name} ${age}`);
+});
+
+app.delete("/deleteUser", (req: Request, res: Response) => {
+  const { userId } = req.body;
+  res.send(`deleted user id ${userId}`);
+});
+
+app.post("/createUser", (req: Request, res: Response) => {
+  const { name, age, userName, userEmail, phoneNumber, password }: User =
+    req.body;
+
+  fs.writeFileSync(
+    "./user.json",
+    JSON.stringify([
+      {
+        name,
+        age,
+        userName,
+        userEmail,
+        phoneNumber,
+        password,
+      },
+    ])
+  );
+
+  res.send("Successfully created User");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  const users = fs.readFileSync("./user.json", { encoding: "utf8", flag: "r" });
+  res.json(JSON.parse(users));
 });
 
 app.listen(port, () => {
