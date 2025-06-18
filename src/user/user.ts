dc21688f6f2b0c46b9c71c383d1695d8b7b5f791
25LP5306 25LP5306 <25LP5306@admins-iMac.local>
2025-06-18 11:56:33 +0800

    eowcnd

--- src/user/user.ts
+++ src/user/user.ts
@@ -2,50 +2,117 @@ import express, { Request, Response } from "express";
 import fs from "fs-extra";
 import { User } from "../type/type";
 
+const userRouter = express.Router();
 
-const useRouter = express.Router();
-
-const uniquiId = Math.random();
-
-useRouter.put("/updateUser", (req: Request, res: Response) => {
-  const { name, age }: User = req.body;
-  res.send(`updated user ${name} ${age}`);
+userRouter.get("/users", (req: Request, res: Response) => {
+  const users = fs.readFileSync("./user.json", { encoding: "utf8", flag: "r" });
+  res.json(JSON.parse(users));
 });
 
-useRouter.delete("/deleteUser", (req: Request, res: Response) => {
-  const { userId } = req.body;
-  
-  
-  res.send(`deleted user id ${userId}`);
-});
-
-useRouter.post("/createUser", (req: Request, res: Response) => {
+userRouter.post("/createUser", (req: Request, res: Response) => {
   const { name, age, userName, userEmail, phoneNumber, password }: User =
     req.body;
 
-  fs.writeFileSync(
-    "./user.json",
-    JSON.stringify([
-      {
-        name,
-        age,
-        userName,
-        userId: uniquiId,
-        userEmail,
-        phoneNumber,
-        password,
-      },
-    ])
-  );
+  const uniqueId = Math.random();
 
-  useRouter.get("/users", (req: Request, res: Response) => {
-    const users = fs.readFileSync("./user.json", { encoding: "utf8", flag: "r" });
-    res.json(JSON.parse(users));
+  const filePath = "./user.json";
+
+  let users: User[] = [];
+
+  if (fs.existsSync(filePath)) {
+    const existingData = fs.readFileSync(filePath, "utf8");
+    if (existingData.trim().length > 0) {
+      users = JSON.parse(existingData);
+    }
+  }
+
+  users.push({
+    name,
+    age,
+    userId: 1,
+    userName,
+    userEmail,
+    phoneNumber,
+    password,
   });
 
-  res.send("tanii medeelel amjilttai burtgegdlee");
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+
+  res.send("Successfully created User");
 });
 
+userRouter.post("/deleteUser", (req: Request, res: Response) => {
+  const { userId } = req.body;
+  const existingData = fs.readFileSync("./user.json", "utf8");
 
+  const deletedUser = JSON.parse(existingData).filter(
+    (user: any) => user.userId !== userId
+  );
 
-export default useRouter;
+  fs.writeFileSync("./user.json", JSON.stringify(deletedUser, null, 2));
+
+  res.json({
+    userId,
+  });
+});
+
+userRouter.put("/updateUser/:id", (req: Request, res: Response) => {
+  const { name, age, userName, userEmail, phoneNumber }: User = req.body;
+  const { id } = req.params;
+
+  const users = getUsers();
+
+  const foundedUser = users.find((user) => user.userId === Number(id));
+
+  if(!foundedUser) {
+    res.json({ success: false, message: "user not found"})
+    return
+  }
+
+  const updatedUsers = users.map((user: User) => {
+    if (user.userId === Number(id)) {
+      return { ...user, name, age, userEmail, userName, phoneNumber };
+    } 
+    
+    return user
+  });
+
+  const updatedUsersJson = updateUsersJson(updatedUsers);
+
+  res.json(updatedUsersJson);
+});
+
+//Selectid
+userRouter.get("/getUser/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const existingData = fs.readFileSync("./user.json", "utf8");
+  const user = JSON.parse(existingData).find(
+    (use: any) => use.userId == userId
+  );
+  if (user) {
+    res.json(user);
+  } else {
+    res.send("hooson");
+  }
+});
+
+export default userRouter;
+
+const getUsers = () => {
+  const usersJson = fs.readFileSync("./user.json", "utf8");
+
+  const users: User[] = JSON.parse(usersJson);
+
+  return users
+};
+
+const updateUsersJson = (users: User[]) => {
+  const stringifyUsers = JSON.stringify(users);
+
+  try {
+    fs.writeFile("./user.json", stringifyUsers);
+    return { success: true, data: users };
+  } catch (error) {
+    return { success: false, data: null };
+  }
+};
